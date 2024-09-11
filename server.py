42c9a4c4c7a163be6b49ab5a6d145c4217f69a07
CrazyChair69 <Christianschwanse@gmx.de>
2024-09-11 21:48:15 +0200

    Code-Refactoring PyLint

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Server for Web Deployment"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -14,8 +16,8 @@ def render_homepage():
 @app.route('/emotionDetector', methods=["GET"])
 def emotion_analysis():
     """ Analyze text and return emotion detection (analysis result) """
-    textToAnalyze = request.args["textToAnalyze"]
-    analysisResult = emotion_detector(textToAnalyze)
+    text_to_analyze = request.args["textToAnalyze"]
+    analysis_result = emotion_detector(text_to_analyze)
 
     # Required output format:
     # For the given statement, the system response is '<emotion1>': <score1>,
@@ -23,22 +25,22 @@ def emotion_analysis():
     #
     # Required output format when dominant_emotion == None:
     # Invalid text! Please try again.
-    if analysisResult["dominant_emotion"] == None:
+    if analysis_result["dominant_emotion"] is None:
         response = "Invalid text! Please try again!"
     else:
         response = "For the given statement, the system response is"
 
-        for key, value in analysisResult.items():
+        for key, value in analysis_result.items():
             # dominant emotion is mentioned separately as its own sentence.
             if key != "dominant_emotion":
                 response += f" '{key}': {value},"
-            
+
         # Replace last comma with point. If index is -1, no commas found.
         last_comma_index = response.rfind(",")
         if last_comma_index != -1:
             response = response[:last_comma_index] + '.' + response[last_comma_index + 1:]
-        
-        response += f" The dominant emotion is {analysisResult['dominant_emotion']}."
+
+        response += f" The dominant emotion is {analysis_result['dominant_emotion']}."
 
     return response
 
