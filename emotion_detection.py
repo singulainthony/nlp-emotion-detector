542dac3619336b8739d7e5a049109a54f48c6f5e
CrazyChair69 <Christianschwanse@gmx.de>
2024-09-11 17:22:54 +0200

    Code Refactoring: emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,7 +8,8 @@ def emotion_detector(text_to_analyze: str) -> str:
     NOTE: Currently, this code only works with IBM Cloud IDE! 
           Otherwise, ConnectionError occurs.
     """
-    request = requests.post(url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict', 
-                            headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
-                            json={ "raw_document": { "text": text_to_analyze}})
-    return request.text
+    response = requests.post(url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict', 
+                             headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
+                             json = { "raw_document": { "text": text_to_analyze} })
+    
+    return response.text
