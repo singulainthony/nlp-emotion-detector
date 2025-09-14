8966e71640cd0132e5b5ca97578e157cf6a916a4
singulainthony <singulainthony@proton.me>
2025-09-14 12:28:32 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for Web Deployment"""
 
 from flask import Flask, render_template, request
-from emotion_detection.emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 
 app = Flask("Emotion Detection")
@@ -49,3 +49,4 @@ def emotion_analysis() -> str:
 # directly (not imported as a module)
 if __name__ == "__main__":
     app.run(debug = True)
+
